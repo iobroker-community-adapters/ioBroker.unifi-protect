ab1de97814ee45bc64da340bbdabe93f07d52d09
Peter Baumert <peter.baumert@de.abb.com>
2020-01-30 17:55:35 +0100

    First commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"unifi-protect adapter settings": {
+		"en": "Adapter settings for unifi-protect",
+		"de": "Adaptereinstellungen für unifi-protect",
+		"ru": "Настройки адаптера для unifi-protect",
+		"pt": "Configurações do adaptador para unifi-protect",
+		"nl": "Adapterinstellingen voor unifi-protect",
+		"fr": "Paramètres d'adaptateur pour unifi-protect",
+		"it": "Impostazioni dell'adattatore per unifi-protect",
+		"es": "Ajustes del adaptador para unifi-protect",
+		"pl": "Ustawienia adaptera dla unifi-protect",
+		"zh-cn": "unifi-protect的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option 2",
+		"ru": "option2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	}
+};
