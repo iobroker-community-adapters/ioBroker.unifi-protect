5b4a104b617d274e51776feae5537d387820b720
Peter Baumert <peter.baumert@de.abb.com>
2020-02-03 14:56:37 +0100

    test

--- main.js
+++ main.js
@@ -157,6 +157,7 @@ class UnifiProtect extends utils.Adapter {
 			res.on("end", () => {
 				if (res.statusCode == 200) {
 					const cameras = JSON.parse(data).cameras;
+					this.createChannel("cameras");
 					cameras.forEach(camera => {
 						this.createChannel("cameras."+camera.mac,camera.name);
 					});
