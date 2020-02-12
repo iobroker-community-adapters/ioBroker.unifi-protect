b3772939d8f71155232096af40cb871a58b28ee5
Peter Baumert <peter.baumert@de.abb.com>
2020-02-12 17:46:41 +0100

    ...

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ class UnifiProtect extends utils.Adapter {
 		this.on("ready", this.onReady.bind(this));
 		this.on("objectChange", this.onObjectChange.bind(this));
 		this.on("stateChange", this.onStateChange.bind(this));
-		// this.on("message", this.onMessage.bind(this));
+		this.on("message", this.onMessage.bind(this));
 		this.on("unload", this.onUnload.bind(this));
 	}
 
