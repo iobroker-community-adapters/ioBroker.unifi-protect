ca784f8151d3a6b6a64195b4254ca82cc59cd043
Scrounger <scrounger@gmx.net>
2020-08-26 20:21:22 +0200

    settings: fanctree view for motions added

--- admin/index_m.js
+++ admin/index_m.js
@@ -1,23 +1,19 @@
 var secret;
 
-function encrypt(key, value) {
-    var result = '';
-    for (var i = 0; i < value.length; ++i) {
-        result += String.fromCharCode(key[i % key.length].charCodeAt(0) ^ value.charCodeAt(i));
-    }
-    return result;
-}
 
-function decrypt(key, value) {
-    var result = '';
-    for (var i = 0; i < value.length; ++i) {
-        result += String.fromCharCode(key[i % key.length].charCodeAt(0) ^ value.charCodeAt(i));
-    }
-    return result;
+
+// the function loadSettings has to exist ...
+async function load(settings, onChange) {
+    socket.emit('getObject', 'system.config', function (err, obj) {
+        secret = (obj.native ? obj.native.secret : '') || 'Y5JQ6qCfnhysf9NG';
+        loadHelper(settings, onChange);
+    });
+
+    onChange(false);
 }
 
 // the function loadSettings has to exist ...
-function loadHelper(settings, onChange) {
+async function loadHelper(settings, onChange) {
     // example: select elements with id=key and class=value and insert value
     if (!settings) return;
     if (settings.electricityPollingInterval === undefined) settings.electricityPollingInterval = 20;
@@ -43,18 +39,147 @@ function loadHelper(settings, onChange) {
             });
         }
     });
+
+    await createTreeViews(settings, onChange);
+
+
     onChange(false);
-    M.updateTextFields();  // function Materialize.updateTextFields(); to reinitialize all the Materialize labels on the page if you are dynamically adding inputs.
+
+    // reinitialize all the Materialize labels on the page if you are dynamically adding inputs:
+    if (M) M.updateTextFields();
 }
 
-// the function loadSettings has to exist ...
-function load(settings, onChange) {
-    socket.emit('getObject', 'system.config', function (err, obj) {
-        secret = (obj.native ? obj.native.secret : '') || 'Y5JQ6qCfnhysf9NG';
-        loadHelper(settings, onChange);
-    });
+/**
+ * @param {*} settings 
+ * @param {*} onChange 
+ */
+async function createTreeViews(settings, onChange) {
+    for (const key of Object.keys(settings.statesFilter)) {
 
-    onChange(false);
+        try {
+            let tree = []
+
+            // get json data from file
+            const propList = await getUnifiObjects(key);
+
+            let children = [];
+            let expanded = false;
+            for (const child of propList) {
+
+                let selected = false;
+                if (settings.statesFilter[key] && settings.statesFilter[key].includes(child.id)) {
+                    selected = true;
+                    expanded = true;
+                }
+
+                children.push({
+                    title: `<div class="fancytree-item-title-id">${child.id}</div><div class="fancytree-item-title-name">${_(child.name)}</div>`,
+                    key: `${key}.${child.id}`,
+                    selected: selected
+                })
+            }
+
+            tree.push({
+                title: `<div class="fancytree-folder-title-id">${key}</div><div class="fancytree-item-title-name">${_(`root_${key}`)}</div>`,
+                key: key,
+                folder: true,
+                expanded: true,
+                children: children
+            })
+
+
+            $(`#tree_${key}`).fancytree({
+                activeVisible: true,                        // Make sure, active nodes are visible (expanded)
+                aria: true,                                 // Enable WAI-ARIA support
+                autoActivate: true,                         // Automatically activate a node when it is focused using keyboard
+                autoCollapse: false,                        // Automatically collapse all siblings, when a node is expanded
+                autoScroll: false,                          // Automatically scroll nodes into visible area
+                clickFolderMode: 2,                         // 1:activate, 2:expand, 3:activate and expand, 4:activate (dblclick expands)
+                checkbox: true,                             // Show check boxes
+                checkboxAutoHide: false,                    // Display check boxes on hover only
+                debugLevel: 0,                              // 0:quiet, 1:errors, 2:warnings, 3:infos, 4:debug
+                disabled: false,                            // Disable control
+                focusOnSelect: false,                       // Set focus when node is checked by a mouse click
+                escapeTitles: false,                        // Escape `node.title` content for display
+                generateIds: false,                         // Generate id attributes like <span id='fancytree-id-KEY'>
+                keyboard: true,                             // Support keyboard navigation
+                keyPathSeparator: "/",                      // Used by node.getKeyPath() and tree.loadKeyPath()
+                minExpandLevel: 1,                          // 1: root node is not collapsible
+                quicksearch: false,                         // Navigate to next node by typing the first letters
+                rtl: false,                                 // Enable RTL (right-to-left) mode
+                selectMode: 3,                              // 1:single, 2:multi, 3:multi-hier
+                tabindex: "0",                              // Whole tree behaves as one single control
+                titlesTabbable: false,                      // Node titles can receive keyboard focus
+                tooltip: false,                             // Use title as tooltip (also a callback could be specified)
+                // icon: function (event, data) {
+                //     if (data.node.isFolder()) {
+                //         return "unifi.png";
+                //     }
+                // },
+                source:
+                    tree
+                ,
+                click: function (event, data) {
+                    if (data.targetType === 'title' && !data.node.folder) {
+                        data.node.setSelected(!data.node.isSelected());
+                    }
+
+                    if (data.targetType === 'checkbox' && data.node.folder) {
+                        data.node.setExpanded(!data.node.isSelected());
+                    }
+                },
+                select: function (event, data) {
+
+                    // Funktion um alle title auszulesen, kann für Übersetzung verwendet werden -> bitte drin lassen!
+                    // var selKeys = $.map(data.tree.getSelectedNodes(), function (node) {
+                    //     if (node.children === null) {
+                    //         return node.title;
+                    //     }
+                    // });
+                    // console.log(selKeys.join('\n').replace(/_/g, " "));
+
+                    onChange();
+                }
+            });
+
+            // reinitialize all the Materialize labels on the page if you are dynamically adding inputs:
+            if (M) M.updateTextFields();
+            
+        } catch (err) {
+            console.error(`[createTreeViews] key: ${key} error: ${err.message}, stack: ${err.stack}`);
+        }
+    }
+}
+
+/**
+ * @param {*} lib 
+ */
+async function getUnifiObjects(lib) {
+    return new Promise((resolve, reject) => {
+        $.getJSON(`./lib/objects_${lib}.json`, function (json) {
+            if (json) {
+                resolve(json);
+            } else {
+                resolve(null);
+            }
+        });
+    });
+}
+
+function encrypt(key, value) {
+    var result = '';
+    for (var i = 0; i < value.length; ++i) {
+        result += String.fromCharCode(key[i % key.length].charCodeAt(0) ^ value.charCodeAt(i));
+    }
+    return result;
+}
+
+function decrypt(key, value) {
+    var result = '';
+    for (var i = 0; i < value.length; ++i) {
+        result += String.fromCharCode(key[i % key.length].charCodeAt(0) ^ value.charCodeAt(i));
+    }
+    return result;
 }
 
 // This will be called by the admin adapter when the user presses the save button
--- admin/words.js
+++ admin/words.js
@@ -1,7 +1,7 @@
 /*global systemDictionary:true */
 "use strict";
 
-systemDictionary = {
+const dictionary = {
 	"unifi-protect adapter settings": {
 		"en": "Adapter settings for unifi-protect",
 		"de": "Adaptereinstellungen für unifi-protect",
@@ -14,28 +14,158 @@ systemDictionary = {
 		"pl": "Ustawienia adaptera dla unifi-protect",
 		"zh-cn": "unifi-protect的适配器设置"
 	},
-	"option1": {
-		"en": "option1",
-		"de": "Option 1",
-		"ru": "Опция 1",
-		"pt": "Opção 1",
-		"nl": "Optie 1",
-		"fr": "Option 1",
-		"it": "opzione 1",
-		"es": "Opción 1",
-		"pl": "opcja 1",
-		"zh-cn": "选项1"
+	"get all your data from your cameras": {
+		"en": "get all your data from your cameras",
+		"de": "Holt alle Daten von Deinen Kameras",
+		"ru": "получить все свои данные с ваших камер",
+		"pt": "obtenha todos os seus dados de suas câmeras",
+		"nl": "haal al uw gegevens van uw camera's",
+		"fr": "récupérez toutes vos données de vos caméras",
+		"it": "ottenere tutti i dati dalle telecamere",
+		"es": "obtén todos tus datos de tus cámaras",
+		"pl": "uzyskać wszystkie dane z kamer",
+		"zh-cn": "从相机获取所有数据"
 	},
-	"option2": {
-		"en": "option2",
-		"de": "Option 2",
-		"ru": "option2",
-		"pt": "opção 2",
-		"nl": "Optie 2",
-		"fr": "Option 2",
-		"it": "opzione 2",
-		"es": "opcion 2",
-		"pl": "Opcja 2",
-		"zh-cn": "选项2"
+	"IP Address / Hostname": {
+		"en": "IP Address / Hostname",
+		"de": "IP-Adresse / Hostname",
+		"ru": "IP-адрес / имя хоста",
+		"pt": "Endereço IP / Nome do host",
+		"nl": "IP-adres / hostnaam",
+		"fr": "Adresse IP / nom d'hôte",
+		"it": "Indirizzo IP / nome host",
+		"es": "Dirección IP / Nombre de host",
+		"pl": "Adres IP / nazwa hosta",
+		"zh-cn": "IP地址/主机名"
+	},
+	"Username": {
+		"en": "Username",
+		"de": "Benutzername",
+		"ru": "Имя пользователя",
+		"pt": "Nome do usuário",
+		"nl": "Gebruikersnaam",
+		"fr": "Nom d'utilisateur",
+		"it": "Nome utente",
+		"es": "Nombre de usuario",
+		"pl": "Nazwa Użytkownika",
+		"zh-cn": "用户名"
+	},
+	"Password": {
+		"en": "Password",
+		"de": "Passwort",
+		"ru": "пароль",
+		"pt": "Senha",
+		"nl": "Wachtwoord",
+		"fr": "Mot de passe",
+		"it": "Parola d'ordine",
+		"es": "Contraseña",
+		"pl": "Hasło",
+		"zh-cn": "密码"
+	},
+	"Refresh Interval": {
+		"en": "Refresh Interval",
+		"de": "Aktualisierungsintervall",
+		"ru": "Интервал обновления",
+		"pt": "Intervalo de atualização",
+		"nl": "Vernieuwingsinterval",
+		"fr": "Intervalle de rafraîchissement",
+		"it": "Intervallo di aggiornamento",
+		"es": "Intervalo de actualización",
+		"pl": "Częstotliwość odświeżania",
+		"zh-cn": "刷新间隔"
+	},
+	"Get Motions (last Motion is always included)": {
+		"en": "Get Motions (last Motion is always included)",
+		"de": "Bewegungen abrufen (letzte Bewegung ist immer enthalten)",
+		"ru": "Получить движения (всегда включается последнее движение)",
+		"pt": "Obter movimentos (o último movimento está sempre incluído)",
+		"nl": "Get Motions (laatste beweging is altijd inbegrepen)",
+		"fr": "Obtenir des motions (la dernière motion est toujours incluse)",
+		"it": "Ottieni movimenti (l'ultimo movimento è sempre incluso)",
+		"es": "Obtener movimientos (el último movimiento siempre está incluido)",
+		"pl": "Pobierz ruchy (ostatni ruch jest zawsze uwzględniany)",
+		"zh-cn": "取得动作（总是包含最后动作）"
+	},
+	"Number of Motions": {
+		"en": "Number of Motions",
+		"de": "Anzahl der Bewegungen",
+		"ru": "Количество движений",
+		"pt": "Número de moções",
+		"nl": "Aantal bewegingen",
+		"fr": "Nombre de motions",
+		"it": "Numero di movimenti",
+		"es": "Número de mociones",
+		"pl": "Liczba ruchów",
+		"zh-cn": "动作数"
+	},
+	"Last x Seconds of Motions": {
+		"en": "Last x Seconds of Motions",
+		"de": "Letzte x Sekunden der Bewegungen",
+		"ru": "Последние x секунд движения",
+		"pt": "Últimos x segundos de movimentos",
+		"nl": "Laatste x seconden van bewegingen",
+		"fr": "Dernières x secondes de motions",
+		"it": "Ultimi x secondi di movimenti",
+		"es": "Últimos x segundos de movimientos",
+		"pl": "Ostatnie x sekund ruchu",
+		"zh-cn": "最后x秒的动作"
+	},
+	"general": {
+		"en": "general",
+		"de": "Allgemeines",
+		"ru": "Общее",
+		"pt": "geral",
+		"nl": "algemeen",
+		"fr": "général",
+		"it": "generale",
+		"es": "general",
+		"pl": "generał",
+		"zh-cn": "一般"
+	},
+	"cameras": {
+		"en": "cameras",
+		"de": "Kameras",
+		"ru": "камеры",
+		"pt": "máquinas fotográficas",
+		"nl": "camera's",
+		"fr": "appareils photo",
+		"it": "macchine fotografiche",
+		"es": "cámaras",
+		"pl": "kamery",
+		"zh-cn": "摄影机"
+	},
+	"motions": {
+		"en": "motions",
+		"de": "Bewegungen",
+		"ru": "движения",
+		"pt": "movimentos",
+		"nl": "bewegingen",
+		"fr": "mouvements",
+		"it": "moti",
+		"es": "mociones",
+		"pl": "ruchy",
+		"zh-cn": "动作"
+	},
+	"root_motions": {
+		"en": "motions datapoints",
+		"de": "Bewegungsdatenpunkte",
+		"ru": "точки данных",
+		"pt": "pontos de dados de movimentos",
+		"nl": "motions datapunten",
+		"fr": "points de données de mouvements",
+		"it": "datapoint dei movimenti",
+		"es": "puntos de datos de movimientos",
+		"pl": "punkty danych ruchu",
+		"zh-cn": "运动数据点"
 	}
+
 };
+
+try {
+	// @ts-ignore
+	systemDictionary = dictionary;
+} catch (ignore) { }
+
+try {
+	module.exports = dictionary;
+} catch (ignore) { }
