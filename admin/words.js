b1bda03b766064411baf03e2f141885a9401c9d5
Scrounger <scrounger@gmx.net>
2020-08-28 08:52:54 +0200

    translation added

--- admin/words.js
+++ admin/words.js
@@ -157,8 +157,187 @@ const dictionary = {
 		"es": "puntos de datos de movimientos",
 		"pl": "punkty danych ruchu",
 		"zh-cn": "运动数据点"
+	},
+	"root_cameras": {
+		"en": "cameras",
+		"de": "Kameras",
+		"ru": "камеры",
+		"pt": "máquinas fotográficas",
+		"nl": "camera's",
+		"fr": "appareils photo",
+		"it": "macchine fotografiche",
+		"es": "cámaras",
+		"pl": "kamery",
+		"zh-cn": "摄影机"
+	},
+	"root_channels": {
+		"en": "channels",
+		"de": "Kanäle",
+		"ru": "каналы",
+		"pt": "canais",
+		"nl": "kanalen",
+		"fr": "canaux",
+		"it": "canali",
+		"es": "canales",
+		"pl": "kanały",
+		"zh-cn": "频道"
+	},
+	"root_featureFlags": {
+		"en": "feature flags",
+		"de": "Feature-Flags",
+		"ru": "флаги функций",
+		"pt": "sinalizadores de recursos",
+		"nl": "kenmerk vlaggen",
+		"fr": "indicateurs de fonction",
+		"it": "flag di funzionalità",
+		"es": "banderas de características",
+		"pl": "flagi funkcji",
+		"zh-cn": "功能标志"
+	},
+	"root_ispSettings": {
+		"en": "isp settings",
+		"de": "ISP-Einstellungen",
+		"ru": "настройки провайдера",
+		"pt": "configurações ISP",
+		"nl": "isp-instellingen",
+		"fr": "paramètres ISP",
+		"it": "impostazioni isp",
+		"es": "configuración de isp",
+		"pl": "isp settings",
+		"zh-cn": "isp设置"
+	},
+	"root_lastMotion": {
+		"en": "last motion",
+		"de": "letzte Bewegung",
+		"ru": "последнее движение",
+		"pt": "último movimento",
+		"nl": "laatste motie",
+		"fr": "dernier mouvement",
+		"it": "ultimo movimento",
+		"es": "último movimiento",
+		"pl": "ostatni ruch",
+		"zh-cn": "最后动议"
+	},
+	"root_ledSettings": {
+		"en": "led settings",
+		"de": "LED-Einstellungen",
+		"ru": "светодиодные настройки",
+		"pt": "configurações de led",
+		"nl": "led instellingen",
+		"fr": "paramètres LED",
+		"it": "impostazioni led",
+		"es": "ajustes de led",
+		"pl": "ustawienia led",
+		"zh-cn": "LED设置"
+	},
+	"root_motionZones": {
+		"en": "motionzones",
+		"de": "Bewegungszonen",
+		"ru": "зоны движения",
+		"pt": "zonas de movimento",
+		"nl": "bewegingszones",
+		"fr": "zones de mouvement",
+		"it": "zone di movimento",
+		"es": "zonas de movimiento",
+		"pl": "strefy ruchu",
+		"zh-cn": "运动区"
+	},
+	"root_osdSettings": {
+		"en": "osd settings",
+		"de": "OSD-Einstellungen",
+		"ru": "настройки OSD",
+		"pt": "configurações de osd",
+		"nl": "osd-instellingen",
+		"fr": "paramètres osd",
+		"it": "impostazioni OSD",
+		"es": "configuración de osd",
+		"pl": "ustawienia osd",
+		"zh-cn": "osd设置"
+	},
+	"root_pirSettings": {
+		"en": "pir settings",
+		"de": "Pir-Einstellungen",
+		"ru": "настройки pir",
+		"pt": "configurações pir",
+		"nl": "pir-instellingen",
+		"fr": "paramètres pir",
+		"it": "impostazioni pir",
+		"es": "configuración pir",
+		"pl": "ustawienia pir",
+		"zh-cn": "速度设定"
+	},
+	"root_recordingSettings": {
+		"en": "recording settings",
+		"de": "Aufnahmeeinstellungen",
+		"ru": "настройки записи",
+		"pt": "configurações de gravação",
+		"nl": "opname-instellingen",
+		"fr": "paramètres d'enregistrement",
+		"it": "impostazioni di registrazione",
+		"es": "configuración de grabación",
+		"pl": "ustawienia nagrywania",
+		"zh-cn": "录音设置"
+	},
+	"root_speakerSettings": {
+		"en": "speaker settings",
+		"de": "Lautsprechereinstellungen",
+		"ru": "настройки динамика",
+		"pt": "configurações de alto-falante",
+		"nl": "luidsprekerinstellingen",
+		"fr": "paramètres des enceintes",
+		"it": "impostazioni degli altoparlanti",
+		"es": "configuración de los altavoces",
+		"pl": "ustawienia głośników",
+		"zh-cn": "扬声器设置"
+	},
+	"root_stats": {
+		"en": "stats",
+		"de": "Statistiken",
+		"ru": "статистика",
+		"pt": "Estatísticas",
+		"nl": "statistieken",
+		"fr": "Statistiques",
+		"it": "statistiche",
+		"es": "estadisticas",
+		"pl": "statystyki",
+		"zh-cn": "统计资料"
+	},
+	"root_talkbackSettings": {
+		"en": "talkback settings",
+		"de": "Talkback-Einstellungen",
+		"ru": "настройки двусторонней связи",
+		"pt": "configurações de talkback",
+		"nl": "talkback-instellingen",
+		"fr": "paramètres de talkback",
+		"it": "impostazioni talkback",
+		"es": "configuración de talkback",
+		"pl": "ustawienia talkback",
+		"zh-cn": "对讲设定"
+	},
+	"root_wifiConnectionState": {
+		"en": "wifi connection state",
+		"de": "WLAN-Verbindungsstatus",
+		"ru": "состояние подключения Wi-Fi",
+		"pt": "estado de conexão wi-fi",
+		"nl": "wifi-verbinding staat",
+		"fr": "état de la connexion wifi",
+		"it": "stato della connessione wifi",
+		"es": "estado de conexión wifi",
+		"pl": "stan połączenia Wi-Fi",
+		"zh-cn": "wifi连接状态"
+	},
+	"root_wiredConnectionState": {
+		"en": "wired connection state",
+		"de": "Kabelverbindungsstatus",
+		"ru": "состояние проводного подключения",
+		"pt": "estado de conexão com fio",
+		"nl": "bekabelde verbinding staat",
+		"fr": "état de la connexion filaire",
+		"it": "stato di connessione cablata",
+		"es": "estado de conexión cableada",
+		"pl": "stan połączenia przewodowego",
+		"zh-cn": "有线连接状态"
 	}
-
 };
 
 try {
